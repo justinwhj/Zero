397b7c6967aaa911c18cb7a90a87de746c3a0226
justinwhj <37104321+justinwhj@users.noreply.github.com>
2020-07-30 12:34:14 +0800

    add adminLTE as its font

--- controller/web_controller.py
+++ controller/web_controller.py
@@ -8,7 +8,7 @@ app = Flask(__name__,template_folder="../templates",static_folder="../static")
 
 @app.route("/")
 def hello(name=None):
-    return render_template("index.html",name=name)
+    return render_template("index_0.html",name=name)
 
 @app.route("/computers/add_computer",methods=["POST"])
 def radd_computer():
@@ -19,7 +19,7 @@ def radd_computer():
     add_computer(computer)
     return redirect(url_for("rquery_computer"))
 
-@app.route("/computers/delete_computer",methods=["POST"])
+@app.route("/computers/delete_computer",methods=["POST","GET"])
 def rdel_computer():
     id = request.values.get("id")
     del_computer(id)
@@ -35,16 +35,24 @@ def rmod_computer():
     mod_computer(new_computer)
     return redirect(url_for("rquery_computer"))
 
-@app.route("/computers/find_computer",methods=["POST"])
+@app.route("/computers/find_computer",methods=["POST","GET"])
 def rfind_computer():
+    old_computer = {
+        "id": 0,
+        "name":"",
+        "host":""
+    }
+    action = "add"
     id = request.values.get("id")
-    old_computer = query_one_computer(id)
-    return render_template("computer_modify.html",old_computer=old_computer)
+    if int(id)!=0:
+        old_computer = query_one_computer(id)
+        action = "modify"
+    return render_template("computer_modify_0.html",old_computer=old_computer,action=action)
 
 @app.route("/computers/query_computer",methods=["POST","GET"])
 def rquery_computer():
     computers = query_all_computer()
-    return render_template("computer.html",computers=computers)
+    return render_template("computer_0.html",computers=computers)
 
 @app.route("/models/add_model",methods=["POST"])
 def radd_model():
@@ -56,7 +64,7 @@ def radd_model():
     add_model(model)
     return redirect(url_for("rque_model"))
 
-@app.route("/models/delete_model",methods=["POST"])
+@app.route("/models/delete_model",methods=["POST","GET"])
 def rdel_model():
     id = request.values.get("id")
     del_model(id)
@@ -73,16 +81,27 @@ def rmod_model():
     mod_model(new_model)
     return redirect(url_for("rque_model"))
 
-@app.route("/models/find_model",methods=["POST"])
+@app.route("/models/find_model",methods=["POST","GET"])
 def rfind_model():
+    action = "add"
+    old_model = {
+        "id": 0,
+        "name": "",
+        "type": "",
+        "location": ""
+    }
+
     id = request.values.get("id")
-    old_model = query_one_model(id)
-    return render_template("model_modify.html",old_model=old_model)
+    if int(id)!=0:
+        # 更新模型
+        old_model = query_one_model(id)
+        action = "modify"
+    return render_template("model_modify_0.html",old_model=old_model,action=action)
 
 @app.route("/models/query_model",methods=["POST","GET"])
 def rque_model():
     models = query_all_models()
-    return render_template("model.html",models=models)
+    return render_template("model_0.html",models=models)
 
 @app.route("/passwords/add_dict",methods=["POST"])
 def radd_dict():
@@ -94,7 +113,7 @@ def radd_dict():
     add_pswd(password)
     return redirect(url_for("rque_dict"))
 
-@app.route("/passwords/del_dict",methods=["POST"])
+@app.route("/passwords/del_dict",methods=["POST","GET"])
 def rdel_dict():
     id = request.values.get("id")
     del_pswd(id)
@@ -111,16 +130,26 @@ def rmod_dict():
     mod_pswd(new_password)
     return redirect(url_for("rque_dict"))
 
-@app.route("/passwords/find_dict",methods=["POST"])
+@app.route("/passwords/find_dict",methods=["POST","GET"])
 def rfind_dict():
+    old_dict = {
+        "id": 0,
+        "name":"",
+        "type":"",
+        "location":""
+    }
+    action = "add"
     id = request.values.get("id")
-    old_dict = query_one_pswd(id)
-    return render_template("password_modify.html",old_dict=old_dict)
+
+    if int(id)!=0:
+        old_dict = query_one_pswd(id)
+        action = "mod"
+    return render_template("password_modify_0.html",old_dict=old_dict,action=action)
 
 @app.route("/passwords/que_dict",methods=["POST","GET"])
 def rque_dict():
     passwords = query_all_pswd()
-    return render_template("password.html",passwords=passwords)
+    return render_template("password_0.html",passwords=passwords)
 
 
 if __name__=="__main__":
